2fdc7ef687fcc9c07399cd5405c6bef52958b15a
metinozkan <metin.ozkn@hotmail.com>
2022-05-27 16:30:23 +0300

    add template, routing and create partials

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
+//template engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("İndex sayafası");
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 app.listen(port, () => {
-  console.log("app start");
+  console.log('app start');
 });
