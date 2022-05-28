1b94b3d893e63842a66dbf4c44b76d4cc1eb4aec
metinozkan <metin.ozkn@hotmail.com>
2022-05-28 21:14:37 +0300

    loginPage, loginUser controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 exports.createUser = async (req, res) => {
   try {
@@ -9,6 +10,32 @@ exports.createUser = async (req, res) => {
       user,
     });
   } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  console.log('loginUser');
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            res.status(200).send('You ARE LOGGED IN');
+          } else {
+            res.status(400).send('Password wrong');
+          }
+        });
+      }
+    }
+  } catch (error) {
+    console.log('loginUser gataya düştü ya', req.body);
     res.status(400).json({
       status: 'fail',
       error,
