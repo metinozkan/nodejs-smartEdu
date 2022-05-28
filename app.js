e9793755f55c7140e11bd623aba3d81d8f340700
metinozkan <metin.ozkn@hotmail.com>
2022-05-28 17:02:35 +0300

    create courseRoute, courseController, createCourse, getAllcourses reqs

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute.js');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//Connect db
+mongoose.connect('mongodb://localhost:27017/smartedu-db', {}).then(() => {
+  console.log('db conencted succesfully');
+});
 
 //template engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
 const port = 3000;
 
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 app.listen(port, () => {
   console.log('app start');
