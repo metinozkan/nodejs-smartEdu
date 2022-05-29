3a29cd865a74f85cfcf84e4190efccfcaac57d53
metinozkan <metin.ozkn@hotmail.com>
2022-05-29 16:28:30 +0300

    session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRoute = require('./routes/pageRoute.js');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -15,14 +16,28 @@ mongoose.connect('mongodb://localhost:27017/smartedu-db', {}).then(() => {
 //template engine
 app.set('view engine', 'ejs');
 
+//global variable
+global.userIN = null;
+
 //Middlewares
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: 'my_keyboard_cat', // Buradaki texti değiştireceğiz.
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 // app.use(cookieParser());
 
 const port = 3000;
 
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/category', categoryRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,8 @@ exports.loginUser = async (req, res) => {
         bcrypt.compare(password, user.password, (err, same) => {
           if (same) {
             // USER SESSION
-            res.status(200).send('You ARE LOGGED IN');
+            req.session.userID = user._id;
+            res.status(200).redirect('/');
           } else {
             res.status(400).send('Password wrong');
           }
